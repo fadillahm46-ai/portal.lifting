334606c6f6ef47f1fbffa8edc90bfdec723309bb
fadillahm46-ai <fadillahm46@gmail.com>
2026-09-08 22:51:06 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'lifting-app-v2'; // Naikkan versi cache
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
